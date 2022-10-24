2932f28a5c0b7286d973a3b9ca8d8f4f1bef333a
David B Ludlow <5985136+davidbludlow@users.noreply.github.com>
2022-10-24 05:05:58 +0000

    Added reception address

--- src/components/reception-details.vue
+++ src/components/reception-details.vue
@@ -1,6 +1,12 @@
 <template>
   <p>Nov 5, 2022 at 5:00 PM</p>
-  <p>Location is TBD</p>
+  <address>
+    <a href="https://goo.gl/maps/1WYdcoXTpj7sCzou7" target="_blank">
+      North 100 East<br />
+      American Fork, UT
+    </a>
+  </address>
+  <p></p>
   <p>Please <a href="javascript:void(0)" v-on:click="goToRsvp()">RSVP</a></p>
 </template>
 
