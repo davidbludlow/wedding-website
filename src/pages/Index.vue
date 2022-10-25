1f6317336cb71b9d748a058bba0b3a1b1257782f
David B Ludlow <5985136+davidbludlow@users.noreply.github.com>
2022-10-25 23:04:35 +0000

    Added a placeholder for home page picture

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -24,7 +24,18 @@
         >
           Geneise Morrison & David Ludlow are getting married!
         </p>
-        <img src="/pictures/website_banner.png" />
+        <!-- <q-img
+          src="/pictures/website_banner_resized_5_percent.png"
+        >
+          <img
+            src="/pictures/website_banner.png"
+          />
+        </q-img> -->
+        <q-img
+          src="/pictures/website_banner.png"
+          placeholder-src="/pictures/website_banner_resized_5_percent.png"
+          :ratio="5649 / 2183"
+        />
         <div class="text-h6">Reception Information</div>
         <ReceptionDetails />
         <q-banner style="text-align: right; font-style: italic">
