e3f1acd8226bd00ca6ea2b88a78d215a95c6da4a
David B Ludlow <5985136+davidbludlow@users.noreply.github.com>
2025-06-30 18:28:51 -0600

    Blend utility type added

--- src/blend.ts
+++ src/blend.ts
@@ -1,12 +1,7 @@
-export type Blend<T extends object[]> = {
-  [K in keyof T]: (
-    & T[K]
-    & { [P in Exclude<keyof UnionExceptIndex<T, K & number>, keyof T[K]>]?: never }
-  );
-}[number];
-
-// Helper: union of all objects except the one at index I
-type UnionExceptIndex<T extends any[], I extends number> = Exclude<
-  T[number],
-  T[I]
->;
+// type Blend<T1, T2> =
+//   | (T1 & { [K in Exclude<keyof T2, keyof T1>]?: never })
+//   | (T2 & { [K in Exclude<keyof T1, keyof T2>]?: never });
+type Blend<T1, T2, T3 = never> =
+  | (T1 & { [K in Exclude<keyof T2 | keyof T3, keyof T1>]?: never })
+  | (T2 & { [K in Exclude<keyof T1 | keyof T3, keyof T2>]?: never })
+  | (T3 & { [K in Exclude<keyof T1 | keyof T2, keyof T3>]?: never });
